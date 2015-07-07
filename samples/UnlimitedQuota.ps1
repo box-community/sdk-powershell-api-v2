15f636f1d0eeff952397385b766f877006107e4b
Jeff Whitworth <jwhitwo@gmail.com>
2015-07-07 08:53:23 -0400

    Added new sample file
    This sample file shows how to use the Box Powershell SDKv2 to configure
    all users with Box to have an unlimited quota.

---
+++ samples/UnlimitedQuota.ps1
@@ -0,0 +1,22 @@
+﻿# UnlimitedQuota.ps1
+#
+# This will set all users within a Box tenant to have an unlimited quota
+
+
+## Edit below this line before use ##
+
+$c_id = "" #replace with your client id
+$c_secret = "" #replace with your client secret
+
+Import-Module C:\Box\Box.psm1 #replace with the path to Box.psm1
+
+## Edit above this line before use ##
+
+$dev_token = Get-BoxToken -clientID $c_id -client_secret $c_secret
+
+$users = Get-BoxAllUsers -token $dev_token
+
+foreach($user in $users)
+{
+    Set-BoxUser -id $user.id -quota -1 -token $dev_token
+}
