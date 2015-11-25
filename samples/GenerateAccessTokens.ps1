c44929b7febc6d2ac30f1b405c9076ecbe83d694
Jeff Whitworth <jnwhitwo@uncg.edu>
2015-11-25 13:27:53 -0500

    Created example for access tokens
    The example will create the initial access tokens.

---
+++ samples/GenerateAccessTokens.ps1
@@ -0,0 +1,13 @@
+﻿# This script will aquire a new authorization code and access tokens. This will launch a browser 
+# and request that you authorize your app to grant the oAuth code. This must happen quickly due
+# to timeouts (30 seconds). Paste the return URL in the powershell windows when prompted and 
+# the tokens will be aquired using the code and stored in the registry. Creating the registry 
+# keys will require the script be run as an administrator.
+
+
+$c_id = "<your client ID>"
+$c_secret = "<your client secret>"
+
+$code = New-BoxoAUTHCode -clientID $c_id
+
+New-BoxAccessTokens -clientID $c_id -client_secret $c_secret -code $code
