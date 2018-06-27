f740e31174a2de638dad9445f28ca29da4667272
Jeff Whitworth <jnwhitwo@uncg.edu>
2018-06-27 17:35:18 -0400

    Created a BoxContent psm1 and front loaded with a few cmdlets.

---
+++ BoxContent/BoxContent.psm1
@@ -0,0 +1,45 @@
+#retuns the specified folder
+function Get-BoxFolder($token, $id)
+{
+    $uri = "https://api.box.com/2.0/folders/$id"
+    $headers = @{"Authorization"="Bearer $token"}
+    $return = Invoke-RestMethod -Uri $uri -Method Get -Headers $headers -ContentType "application/x-www-form-urlencoded"
+    
+    return $return
+}
+
+#returns the items contained in a specified folder
+function Get-BoxFolderItems($token, $id)
+{
+    $uri = "https://api.box.com/2.0/folders/$id/items"
+    $headers = @{"Authorization"="Bearer $token"}
+    $return = Invoke-RestMethod -Uri $uri -Method Get -Headers $headers -ContentType "application/x-www-form-urlencoded"
+    
+    return $return.entries
+}
+
+#returns the folder collaborators
+function Get-BoxFolderCollabs($token, $id)
+{
+    $uri = "https://api.box.com/2.0/folders/$id/collaborations"
+    $headers = @{"Authorization"="Bearer $token"}
+    $return = Invoke-RestMethod -Uri $uri -Method Get -Headers $headers -ContentType "application/x-www-form-urlencoded"
+    
+    return $return.entries
+}
+
+#creates a new folder under the provided parent
+function New-BoxFolder($token, $parent, $name)
+{
+    $uri = "https://api.box.com/2.0/folders"
+    $headers = @{"Authorization"="Bearer $token"}
+
+    $json = '{'
+    $json += '"name": "' + $name + '",'
+    $json += '"parent": {"id":"' + $parent + '"}'
+    $json += '}'
+
+    $result = Invoke-RestMethod -Uri $uri -Method Post -Headers $headers -Body $json -ContentType "application/json"
+
+    return $result.id
+}
